57e73dc61bd094112e2bb84d8a46fbce33546492
App Generator <51070104+app-generator@users.noreply.github.com>
2025-09-27 03:04:50 +0300

    initial commit

---
+++ home/models.py
@@ -0,0 +1,47 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    #__PROFILE_FIELDS__
+    investigator = models.TextField(max_length=255, null=True, blank=True)
+    time active = models.DateTimeField(blank=True, null=True, default=timezone.now)
+
+    #__PROFILE_FIELDS__END
+
+    def __str__(self):
+        return self.user.username
+    
+    class Meta:
+        verbose_name        = _("UserProfile")
+        verbose_name_plural = _("UserProfile")
+
+#__MODELS__
+class Whistleform(models.Model):
+
+    #__Whistleform_FIELDS__
+    name = models.CharField(max_length=255, null=True, blank=True)
+    amount = models.TextField(max_length=255, null=True, blank=True)
+    date = models.DateTimeField(blank=True, null=True, default=timezone.now)
+    ongoing = models.BooleanField()
+
+    #__Whistleform_FIELDS__END
+
+    class Meta:
+        verbose_name        = _("Whistleform")
+        verbose_name_plural = _("Whistleform")
+
+
+
+#__MODELS__END
